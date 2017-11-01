b12b2077e352d0ccb536d0f522e6ec58a018f543
alepuente <ale3puente@gmail.com>
2017-10-31 22:13:23 -0300

    Ships upgrade system integrated

--- DV2017/Assets/Scripts/Enemies/EnemyController.cs
+++ DV2017/Assets/Scripts/Enemies/EnemyController.cs
@@ -6,7 +6,6 @@ using UnityEngine.AI;
 public class EnemyController : MonoBehaviour
 {
     private int attackPosition;
-    private PlayerController player;
     public float speed;
     public float turnSpeed;
     private Vector3 attackPos;
@@ -14,18 +13,17 @@ public class EnemyController : MonoBehaviour
 
     void Start()
     {
-        player = PlayerController.instance;
-        attackPosition = Random.Range(0, player.attackPoint.Length);
+        attackPosition = Random.Range(0, PlayerController.instance.attackPoint.Length);
         speed = GameManager.instance.SpeedDic[name];
         turnSpeed = GameManager.instance.TurnSpeedDic[name];
     }
 
     void Update()
     {
-        if (Vector3.Distance(transform.position, player.attackPoint[attackPosition].transform.position) > 0)
+        if (Vector3.Distance(transform.position, PlayerController.instance.attackPoint[attackPosition].transform.position) > 0)
         {
             transform.position += transform.forward * speed * Time.deltaTime;
-            Vector3 targetDir = player.attackPoint[attackPosition].transform.position + new Vector3(Random.Range(0,5),0,0) - transform.position;
+            Vector3 targetDir = PlayerController.instance.attackPoint[attackPosition].transform.position + new Vector3(Random.Range(0,5),0,0) - transform.position;
             float step = Time.deltaTime / Mathf.PI;
             Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step * turnSpeed, 0.0f);
             transform.rotation = Quaternion.LookRotation(new Vector3(newDir.x, 0, newDir.z));       
---
+++ DV2017/Assets/Scripts/ShopItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopItem : MonoBehaviour {
+
+    public int _price;
+    public string _name;
+	
+}
