89eb64d30ccdf39ffa8136b69aae1d88187cefab
Maldorami <ramamaldonado@gmail.com>
2017-12-06 22:04:41 -0300

    Chest por level (solo en level 1) Raycast de movimiento fixeado

---
+++ DV2017/Assets/Scripts/ChestCountOnButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ChestCountOnButton : MonoBehaviour
+{
+
+    public int level;
+    public Button button;
+    public Text countText;
+
+    private void Update()
+    {
+        if (button.IsInteractable())
+        {
+            countText.gameObject.SetActive(true);
+            countText.text = LevelControlScript.instance.startsPerLevel[level - 1].ToString();
+        }
+        else
+        {
+            countText.gameObject.SetActive(false);
+        }
+    }
+}
